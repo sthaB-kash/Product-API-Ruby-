4aa5ef52009bfb828a5274fd111561370392e780
Bikash Shrestha <bikashshrestha@bajratechnologies.com>
2022-02-03 17:32:46 +0545

    feat: product crud

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,5 +1,53 @@
 class Api::V1::ProductsController < ApplicationController
+  before_action :find_product, except: %i[index create]
+
   def index
-    render json: { greeting: 'hello world' }, status: 200
+    @products = Product.all
+    render json: @products
+  end
+
+  def show
+    if @product
+      render json: @product, status: 200
+    else
+      render json: { message: 'Not found!' }, status: 400
+    end
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      render json: { message: 'Product save successfully.' }, status: 200
+    else
+      render json: @product.errors, status: 400
+    end
+  end
+
+  def update
+    if @product
+      @product.update(product_params)
+      render json: @product, status: 200
+    else
+      render json: { message: 'Unable to update product record.' }, status: 400
+    end
+  end
+
+  def destroy
+    if @product
+      @product.destroy
+      render json: { message: 'Product Deleted.' }, status: 200
+    else
+      render json: { message: 'Record not found!' }, status: 400
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit!
+  end
+
+  def find_product
+    @product = Product.find_by_id(params[:id])
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,9 @@ class Product < ApplicationRecord
   belongs_to :supplier
 
   validates_presence_of :name, :amount, :mfd, :exp, :category
+
+  # def date_format(date_value)
+  #   date = date_value.split(%r{[-/:._]})
+  #   Time.local(date[0], date[1], date[2])
+  # end
 end
