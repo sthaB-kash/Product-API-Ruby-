5290cd2c5b814e660de170653b6b70c32619d45d
Bikash Shrestha <bikashshrestha@bajratechnologies.com>
2022-02-07 15:36:03 +0545

    fix: resolved devise-jwt undefined local variable flash for api

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,13 +4,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
   private
 
   def respond_with(resource, _opts = {})
-    resource.persisted? ? register_success : register_failed
+    resource.active_for_authentication? ? register_success : register_failed
   end
 
   def register_success
     render json: { message: 'Signed up.' }
   end
-  
+
   def register_failed
     render json: { message: "Signed up failure." }
   end
